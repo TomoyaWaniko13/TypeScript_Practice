3b9801f5cfa9d295016756aeed51b2bcd726f79b
TomoyaWaniko <alligatorfree12@gmail.com>
2024-04-16 19:57:47 +0900

    Update

--- TypeScript_Tutorial/src/index.ts
+++ TypeScript_Tutorial/src/index.ts
@@ -1,2 +1,15 @@
+import { log } from "console";
+
 console.log('Hello, world!');
+
+function addNumber(a: number, b: number): number {
+    return a + b;
+}
+
+let message = 'Hello, world!'
+console.log(message);
+
+console.log('Hello, world!'.length);
+
+
 
