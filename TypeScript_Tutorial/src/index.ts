1d2b5e393ebad0a1d91ab7329d057f38aea1795b
TomoyaWaniko <alligatorfree12@gmail.com>
2024-05-10 06:40:37 +0900

    update

--- TypeScript_Tutorial/src/index.ts
+++ TypeScript_Tutorial/src/index.ts
@@ -1,15 +1,32 @@
-import { log } from "console";
+let userName: string;
+userName = 'neko-san';
 
-console.log('Hello, world!');
+let userID: string | number = 'abc1';
+userID = 123;
 
-function addNumber(a: number, b: number): number {
-    return a + b;
+let user: {
+  name: string;
+  age: number;
+  isAdmin: boolean;
+  id: string | number;
+};
+
+user = {
+  name: 'Max',
+  age: 10,
+  isAdmin: true,
+  id: 1012913,
+};
+
+let hobbies: Array<String>;
+hobbies = ['Sports', 'Cooking', 'Reading'];
+
+function add(a: number, b: number): number {
+  return a + b;
 }
 
-let message = 'Hello, world!'
-console.log(message);
+const calculate = (a: number, b: number, calcFn: (a: number, b: number) => number) => {
+  calcFn(a, b);
+};
 
-console.log('Hello, world!'.length);
-
-
-
+calculate(1, 2, add);
